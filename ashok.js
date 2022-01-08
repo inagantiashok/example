fa9c0e6bf1f2e879f147eb03df7616e67742f519
inagantiashok <inaganti4u@gamil.com>
2022-01-08 20:11:32 +0530

    first commit

---
+++ ashok.js
@@ -0,0 +1,10 @@
+class docalculation{
+    constructor(){
+        console.log('docalculationn');
+    }
+}
+class docalculation1{
+    constructor(){
+        console.log('docalculationn');
+    }
+    }
